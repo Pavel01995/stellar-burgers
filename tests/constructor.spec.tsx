b161334d6c0191a9117e0df3523ae28e85bebdc3
Pavel <Pavel01995@yandex.kz>
2026-08-29 13:27:08 +0500

    order

--- tests/constructor.spec.tsx
+++ tests/constructor.spec.tsx
@@ -1,54 +1,52 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeEach('проверка конструтора бургера', async ({ page }) => {
-  await page.routeFromHAR('tests/hars/constructor.har', {
-    update: false,
-    url: '**/api/ingredients',
-    notFound: 'abort'
+test.describe('проверка функциональности конструктора', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.routeFromHAR('tests/hars/constructor.har', {
+      update: false,
+      url: '**/api/ingredients',
+      notFound: 'abort'
+    });
+
+    await page.goto('/');
   });
 
-  await page.goto('/');
-});
+  test('добавить булку в конструктора', async ({ page }) => {
+    await page
+      .locator('li:has(a[href*="/ingredients/643d69a5c3f7b9001cfa093c"])')
+      .getByRole('button', { name: 'Добавить' })
+      .click();
+    await expect(
+      page
+        .locator('.constructor-element')
+        .filter({ hasText: 'Краторная булка N-200i' })
+        .first()
+    ).toBeVisible();
+  });
 
+  test('добавить начинку в конструктора', async ({ page }) => {
+    await page
+      .locator('li:has(a[href*="/ingredients/643d69a5c3f7b9001cfa0941"])')
+      .getByRole('button', { name: 'Добавить' })
+      .click();
+    await expect(
+      page
+        .locator('.constructor-element')
+        .filter({ hasText: 'Биокотлета из марсианской Магнолии' })
+        .first()
+    ).toBeVisible();
+  });
 
-
-
-
-test('добавить булку в кострутор', async ({ page }) => {
-  await page
-    .locator('li:has(a[href*="/ingredients/643d69a5c3f7b9001cfa093c"])')
-    .getByRole('button', { name: 'Добавить' })
-    .click();
-  await expect(
-    page
-      .locator('.constructor-element')
-      .filter({ hasText: 'Краторная булка N-200i' })
-      .first()
-  ).toBeVisible();
-});
-
-test('добавить начинку в конструтор', async ({ page }) => {
-  await page
-    .locator('li:has(a[href*="/ingredients/643d69a5c3f7b9001cfa0941"])')
-    .getByRole('button', { name: 'Добавить' })
-    .click();
-  await expect(
-    page
-      .locator('.constructor-element')
-      .filter({ hasText: 'Биокотлета из марсианской Магнолии' })
-      .first()
-  ).toBeVisible();
-});
-
-test('добавить соус в коструктор', async ({ page }) => {
-  await page
-    .locator('li:has(a[href*="/ingredients/643d69a5c3f7b9001cfa0942"])')
-    .getByRole('button', { name: 'Добавить' })
-    .click();
-  await expect(
-    page
-      .locator('.constructor-element')
-      .filter({ hasText: 'Соус Spicy-X' })
-      .first()
-  ).toBeVisible();
+  test('добавить соус в конструктора', async ({ page }) => {
+    await page
+      .locator('li:has(a[href*="/ingredients/643d69a5c3f7b9001cfa0942"])')
+      .getByRole('button', { name: 'Добавить' })
+      .click();
+    await expect(
+      page
+        .locator('.constructor-element')
+        .filter({ hasText: 'Соус Spicy-X' })
+        .first()
+    ).toBeVisible();
+  });
 });
--- tests/modal.spec.tsx
+++ tests/modal.spec.tsx
@@ -1,37 +1,46 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
-  await page.routeFromHAR('tests/hars/constructor.har', {
-    update: false,
-    url: '**/api/ingredients',
-    notFound: 'abort'
+test.describe('проверка модальных окон', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.routeFromHAR('tests/hars/constructor.har', {
+      update: false,
+      url: '**/api/ingredients',
+      notFound: 'abort'
+    });
+    await page.goto('/');
   });
-  await page.goto('/');
-});
 
-test('открытие модального окна игредиента', async ({ page }) => {
-  await page
-    .locator('li:has(a[href*="/ingredients/643d69a5c3f7b9001cfa0942"])')
-    .click();
+  test('открытие модального окна игредиента', async ({ page }) => {
+    await page
+      .locator('li:has(a[href*="/ingredients/643d69a5c3f7b9001cfa0942"])')
+      .click();
 
-  await expect(page.locator('text=Детали ингредиента')).toBeVisible();
-});
-test('закрытие модального окна по клику на крестик', async ({ page }) => {
-  await page
-    .locator('li:has(a[href*="/ingredients/643d69a5c3f7b9001cfa0942"])')
-    .click();
-  await expect(page.locator('text=Детали ингредиента')).toBeVisible();
+    await expect(page.locator('text=Детали ингредиента')).toBeVisible();
+    await expect(
+      page.getByRole('heading', { name: 'Соус Spicy-X' })
+    ).toBeVisible();
+  });
 
-  await page.locator('#modals button').click();
+  test('закрытие модального окна по клику на крестик', async ({ page }) => {
+    await page
+      .locator('li:has(a[href*="/ingredients/643d69a5c3f7b9001cfa0942"])')
+      .click();
+    await expect(page.locator('text=Детали ингредиента')).toBeVisible();
 
-  await expect(page.locator('text=Детали ингредиента')).not.toBeVisible();
-});
+    await page.locator('#modals button').click();
 
-test('закрытие по оверлею', async ({ page }) => {
-  await page
-    .locator('li:has(a[href*="/ingredients/643d69a5c3f7b9001cfa0942"])')
-    .click();
-  await expect(page.locator('text=Детали ингредиента')).toBeVisible();
-  await page.mouse.click(10, 10);
-  await expect(page.locator('text=Детали ингредиента')).not.toBeVisible();
+    await expect(page.locator('text=Детали ингредиента')).not.toBeVisible();
+  });
+
+  test('закрытие по оверлею', async ({ page }) => {
+    await page
+      .locator('li:has(a[href*="/ingredients/643d69a5c3f7b9001cfa0942"])')
+      .click();
+    await expect(page.locator('text=Детали ингредиента')).toBeVisible();
+    await page
+      .locator('#modals > div')
+      .filter({ hasNotText: 'Детали ингредиента' })
+      .click({ position: { x: 0, y: 0 } }); 
+    await expect(page.locator('text=Детали ингредиента')).not.toBeVisible();
+  });
 });
